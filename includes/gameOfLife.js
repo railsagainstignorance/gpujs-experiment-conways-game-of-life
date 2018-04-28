e4b6d5980bad4cd347229008489c3f902a7d3b7d
railsagainstignorance <cgatherc@hotmail.com>
2018-04-28 22:33:36 +0100

    animated offsets

--- includes/gameOfLife.js
+++ includes/gameOfLife.js
@@ -3,21 +3,46 @@ const GOL = (function(){
   function create( config ){
     const gpu = config.gpu;
 
-    const initGrid = gpu.createKernel(function() {
-        return (6 * (this.thread.x % 5)) + (4 * (this.thread.y % 10));
+    const initGrid = gpu.createKernel(function(offset) {
+        return (6 * ((this.thread.x + offset) % 5)) + (4 * ((this.thread.y + offset) % 10));
     }).setOutput([config.sizeX, config.sizeY])
 
     const render = gpu.createKernel(function(a) {
-        this.color(a[this.thread.x], a[this.thread.y], 200, 1);
+        // this.color( 0-1, 0-1, 0-1, 1)
+        this.color(
+          (a[this.thread.x][this.thread.y] % 100)/100,
+          (a[this.thread.x][this.thread.y] % 100)/100,
+          0.1,
+          1
+        );
     })
       .setOutput([config.sizeX, config.sizeY])
       .setGraphical(true);
 
-    const initAndRender = gpu.combineKernels(initGrid, render, function() {
-	     return render(initGrid());
+    const initAndRender = gpu.combineKernels(initGrid, render, function(offset) {
+	     return render(initGrid(offset));
      });
 
-    initAndRender();
+    var offset=5;
+    const maxOffset = 200;
+    function animateOffsets(){
+       initAndRender(offset);
+       const canvas = render.getCanvas();
+       const prevCanvas = config.canvasDivElement.firstChild;
+       if(prevCanvas == null){
+         config.canvasDivElement.appendChild(canvas);
+       } else {
+         config.canvasDivElement.replaceChild(canvas, prevCanvas);
+       }
+       if (offset < maxOffset) {
+         offset += 1;
+         window.requestAnimationFrame(animateOffsets);
+         // window.setTimeout(function(){
+         //   window.requestAnimationFrame(animateOffsets);
+         // }, 1000);
+       }
+     }
+     window.requestAnimationFrame(animateOffsets);
 
     // const grid = initGrid();
     // render(grid);
