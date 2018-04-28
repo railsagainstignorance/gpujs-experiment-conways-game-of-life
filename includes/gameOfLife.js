ac0b5fffdf2f176ea2151c18bd8992d524931b71
railsagainstignorance <cgatherc@hotmail.com>
2018-04-28 21:30:51 +0100

    chained kernels

--- includes/gameOfLife.js
+++ includes/gameOfLife.js
@@ -3,13 +3,25 @@ const GOL = (function(){
   function create( config ){
     const gpu = config.gpu;
 
-    const render = gpu.createKernel(function() {
-        this.color(50 * (this.thread.x % 3), 50 * (this.thread.y % 4), 50, 1);
+    const initGrid = gpu.createKernel(function() {
+        return (6 * (this.thread.x % 5)) + (4 * (this.thread.y % 10));
+    }).setOutput([config.sizeX, config.sizeY])
+
+    const render = gpu.createKernel(function(a) {
+        this.color(a[this.thread.x], a[this.thread.y], 200, 1);
     })
       .setOutput([config.sizeX, config.sizeY])
       .setGraphical(true);
 
-    render();
+    // const initAndRender = gpu.combineKernels(initGrid, render, function() {
+	  //    return render(initGrid());
+    //  });
+    //
+    // initAndRender();
+
+    const grid = initGrid();
+    render(grid);
+
     const canvas = render.getCanvas();
     config.displayCanvas( canvas );
   }
